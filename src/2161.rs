c202f8bf788f2efa2a7679396bf1940aa43c62fd
tanekaha <nicolaswolyniec@gmail.com>
2025-03-03 15:00:52 +0100

    3/3/25 [hooks=30f09b]

---
+++ src/2161.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn pivot_array(nums: Vec<i32>, pivot: i32) -> Vec<i32> {
+        let mut less = Vec::new();
+        let mut greater = Vec::new();
+        let mut same = Vec::new();
+        for n in nums.into_iter(){
+            if n < pivot {
+                less.push(n);
+            } else if n > pivot {
+                greater.push(n);
+            } else {
+                same.push(n);
+            }
+        }
+        less.append(&mut same);
+        less.append(&mut greater);
+        less
+    }
+}
